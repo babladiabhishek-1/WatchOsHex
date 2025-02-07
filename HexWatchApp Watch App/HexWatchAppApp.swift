e94470ce08f3c959f6fc196f45428c9230a494eb
Babladi Abhishek (Contractor) <abhishek.babladi+mcdcorp@us.mcd.com>
2025-02-07 14:22:12 +0100

    Watch app integration
    Added Watch app support

---
+++ HexWatchApp Watch App/HexWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexWatchAppApp.swift
+//  HexWatchApp Watch App
+//
+//  Created by abhishekbabladi on 2025-02-07.
+//
+
+import SwiftUI
+
+@main
+struct HexWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HexWatchApp Watch AppTests/HexWatchApp_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HexWatchApp_Watch_AppTests.swift
+//  HexWatchApp Watch AppTests
+//
+//  Created by abhishekbabladi on 2025-02-07.
+//
+
+import Testing
+@testable import HexWatchApp_Watch_App
+
+struct HexWatchApp_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
