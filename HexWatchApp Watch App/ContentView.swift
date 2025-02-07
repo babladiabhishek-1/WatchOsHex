e9575c5a93b76d912b168bff6d3e77bcf4b9ddb7
Justin <justin.peters@umain.com>
2025-02-07 16:57:32 +0100

    Add in healthkit

--- HexWatchApp Watch App/ContentView.swift
+++ HexWatchApp Watch App/ContentView.swift
@@ -6,18 +6,34 @@
 //
 
 import SwiftUI
+import FirebaseDatabaseInternal
+import HealthKit
 
 struct ContentView: View {
+    
+    let healthStore: HKHealthStore = .init()
+    
+    let ref :DatabaseReference! = Database.database().reference()
+    
+    init () {
+        autorizeHealthKit()
+    }
+    
     var body: some View {
         VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundStyle(.tint)
-//            Text("Hello, world!")
             P266_ViscosityCanvas()
         }
         .padding()
     }
+    
+    func autorizeHealthKit() {
+          
+          // Used to define the identifiers that create quantity type objects.
+            let healthKitTypes: Set = [
+            HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!]
+         // Requests permission to save and read the specified data types.
+            healthStore.requestAuthorization(toShare: healthKitTypes, read: healthKitTypes) { _, _ in }
+        }
 }
 
 #Preview {
