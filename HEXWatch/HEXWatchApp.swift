7298f7d1cd99ffff09c84736e9eaf819c20b8533
Babladi Abhishek (Contractor) <abhishek.babladi+mcdcorp@us.mcd.com>
2025-02-07 14:13:36 +0100

    First commit
    first commit

---
+++ HEXWatch/HEXWatchApp.swift
@@ -0,0 +1,32 @@
+//
+//  HEXWatchApp.swift
+//  HEXWatch
+//
+//  Created by abhishekbabladi on 2025-02-07.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct HEXWatchApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ HEXWatchTests/HEXWatchTests.swift
@@ -0,0 +1,16 @@
+//
+//  HEXWatchTests.swift
+//  HEXWatchTests
+//
+//  Created by abhishekbabladi on 2025-02-07.
+//
+
+import Testing
+
+struct HEXWatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
